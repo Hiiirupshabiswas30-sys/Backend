f756e988892aac555897bc7987ae869c5cef009d
Rupsha <rupshabiswas30@gamil.com>
2026-02-02 01:54:00 +0530

    new | day-7

---
+++ Day-7/src/models/notes.model.js
@@ -0,0 +1,12 @@
+
+const mongoose =require("mongoose")
+
+const noteSchema = new mongoose.Schema({
+    title:  String,
+    description: String,
+    age:String,
+})
+
+const noteModel = mongoose.model("notes", noteSchema)
+
+module.exports=  noteModel
