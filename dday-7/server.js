2742cd34a2701d1140d96502b7566145debd0ec6
Rupsha <rupshabiswas30@gamil.com>
2026-02-06 16:14:10 +0530

    day7 | DONE

--- dday-7/server.js
+++ dday-7/server.js
@@ -1,19 +1,18 @@
-//server ko start karna 
-// database se connect krna
+//server start karna
+//connect with database
+
+//const mongoose = require("mongoose")
 
 require("dotenv").config()
-
 const connectToDb = require("./src/config/database")
-
 const app = require("./src/app")
 
 
 
 connectToDb()
 
-
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
     
-})
 
+})
