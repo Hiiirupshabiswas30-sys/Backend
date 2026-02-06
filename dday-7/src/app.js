38f626c3b57073f64f60173a7daf84b510c2a238
Rupsha <rupshabiswas30@gamil.com>
2026-02-06 16:36:14 +0530

    Add GET

--- dday-7/src/app.js
+++ dday-7/src/app.js
@@ -23,8 +23,21 @@ app.post("/notes", async (req, res) => {
         message: "Note created successfully",
         note
     })
-
-
 })
+
+//GET /notes
+//FETCH ALL THE NOTES FROM DATA
+
+app.get("/notes", async (req, res) => {
+   const notes = await noteModel.find()
+
+   res.status(200).json({
+    message: "Notes fetched successfully",
+    notes
+   })
+      
+})
+
+
 
 module.exports = app
