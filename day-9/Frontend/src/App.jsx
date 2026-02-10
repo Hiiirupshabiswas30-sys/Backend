ba8b6d38b4e35e8c50b4d124eb963aa5b6245995
Rupsha <rupshabiswas30@gamil.com>
2026-02-11 01:23:29 +0530

    update api url

--- day-9/Frontend/src/App.jsx
+++ day-9/Frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   console.log("Hello Integration")
 
   function fetchNotes(){
-       axios.get('http://localhost:3000/api/notes')
+       axios.get("https://backend-9seo.onrender.com/api/notes")
        .then((res)=>{
          setNotes(res.data.notes)
          })
@@ -23,7 +23,7 @@ function App() {
 
     console.log(title.value, description.value)
 
-    axios.post("http://localhost:3000/api/notes",{
+    axios.post("https://backend-9seo.onrender.com/api/notes",{
       title: title.value,
       description: description.value
     })
@@ -35,7 +35,7 @@ function App() {
 
   function handleDeleteNote(noteId){
 
-    axios.delete("http://localhost:3000/api/notes/"+noteId)
+    axios.delete("https://backend-9seo.onrender.com/api/notes/"+noteId)
     .then(res=>{
       console.log(res.data)
       fetchNotes()
